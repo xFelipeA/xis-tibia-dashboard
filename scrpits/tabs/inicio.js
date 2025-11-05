11208a80f184d31dd241a9d16315ac1b91231e08
xFelipeA <Porps85@gmail.com>
2025-11-05 10:24:32 -0300

    Create inicio.js

---
+++ scrpits/tabs/inicio.js
@@ -0,0 +1,13 @@
+class InicioTab {
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        const tabElement = document.getElementById('inicio-tab');
+        
+        // This tab is already populated in the HTML
+        // We can add dynamic content here if needed
+        console.log('✅ Aba Início carregada');
+    }
+}
